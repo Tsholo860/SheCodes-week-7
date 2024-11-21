d558f88b4090629b3bcdf55be6bd2011cd15036b
Tsholo860 <tsholo860@gmail.com>
2024-11-21 14:41:42 +0200

    updated additional java details

--- indes.js
+++ indes.js
@@ -1,8 +1,27 @@
+function updateWeather(response){
+let temperatureElement = document.querySelector(`#temperature`);
+temperatureElement.innerHTML = Math.round(temperature);
+let temperature = response.data.temperature.current;
+    console.log(response.data);
+}
+
+
+
+function searchCity (city){
+let apiKey = `01f1e3c8846fa60fce9eodfad5fbt6b4`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector(`#search-form-input`);
     let cityElement = document.querySelector(`#city`);
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector(`#search-form`);
