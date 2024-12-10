198de9da86260822636792465fd90a0d4dacff98
Tsholo860 <tsholo860@gmail.com>
2024-12-10 19:21:19 +0200

    injected new strings through a loop in Java Script

--- indes.js
+++ indes.js
@@ -21,6 +21,27 @@ function handleSearchSubmit(event){
     searchCity(searchInput.value);
 }
 
+function displayForecast(){
+  let days = [ `Tue` ,   `Wed` , `Thu` , `Fri` , `Sat`];
+  let forecastHtml = "";
+
+  days.forEach ( function (day) {
+forecastHtml = forecastHtml +
+` 
+     <div class="weather-forecast-day">
+                    <div class="forecast-date">${day}</div>
+                    <div class="forecast-emoji">🌦️</div>
+                    <div class="forecast-temperatures">
+                        <div class="forecast-temperature"><strong>13°</strong></div>
+                        <div class="forecast-temperature">6°</div>
+                    </div>
+                </div> 
+                `;
+});
+let forecastElement = document.querySelector(`#forecast`);
+forecastElement.innerHTML = forecastHtml;                
+}
 let searchFormElement = document.querySelector(`#search-form`);
 searchFormElement,addEventListener(`submit`, handleSearchSubmit);
 searchCity(`Paris`);
+displayForecast();
